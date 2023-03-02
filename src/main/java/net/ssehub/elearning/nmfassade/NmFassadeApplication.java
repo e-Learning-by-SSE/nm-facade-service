124f66c309141818098ca2bc86b635be599de667
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-03-02 09:55:34 +0100

    Empty Spring application

---
+++ src/main/java/net/ssehub/elearning/nmfassade/NmFassadeApplication.java
@@ -0,0 +1,13 @@
+package net.ssehub.elearning.nmfassade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmFassadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmFassadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ssehub/elearning/nmfassade/NmFassadeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ssehub.elearning.nmfassade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmFassadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
